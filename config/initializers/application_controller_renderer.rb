5e9f34473a86816f94b45790d05a263fc136fa15
miy5 <miy5alice@gmail.com>
2019-11-11 15:47:07 +0900

    finish

---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,9 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+Refile.secret_key = 'ecb65151c0457d5070249d424f6a609972f1a8247d54c4482d37082dee3cc141604ee0b576b7bf31ab0db80a35528ff555e9be8f4c1281caff96ee23b61fd146'
